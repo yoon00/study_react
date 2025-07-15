89f643e48d85bb7abc6687b1c7fb990172f2d14f
yoon00 <y00ndlek@gmail.com>
2025-07-15 10:49:12 +0900

    0715_react

--- 11-fetching-data/src/components/context/App.tsx
+++ 11-fetching-data/src/components/context/App.tsx
@@ -1,25 +1,31 @@
-import { useState } from "react";
-import Parent from "./Parent";
+import { useState } from "react"
+import Parent from "./Parent"
 import { UserContext } from "./UserContext";
+import { ThemeProvider } from "./ThemeContext";
 
 
-
-// props drilling
 function App() {
-  const [username, setUsername] = useState("u_s_e_r");
+  const [username, setUsername] = useState('심선범');
+
+
   return (
-    <UserContext value={{username}}>
-      <div
-        style={{
-          border: "1px solid gray",
-          padding: "10px",
-        }}
-      >
-        <h1>버튼을 클릭하면 사용자가 변경됩니다.</h1>
-        <Parent username={username} />
-        <button type="button">사용자 변경</button>
-      </div>
-    </UserContext>
-  );
+    <ThemeProvider>
+      <UserContext value={{username,setUsername}}>
+        <div style={{
+          border:'1px solid gray',
+          padding:'10px'
+        }}>
+          <h1>버튼을 클릭하면 사용자가 변경됩니다!</h1>
+          <Parent/>
+          <button 
+            type="button" 
+            onClick={()=>{
+              setUsername('이승은')
+            }
+          }>사용자 변경</button>
+        </div>
+      </UserContext>
+    </ThemeProvider>
+  )
 }
-export default App;
+export default App
