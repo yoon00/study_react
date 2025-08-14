7e6faae266f08cc94b18b61e2cc631421ed5d52e
yoon00 <y00ndlek@gmail.com>
2025-08-14 09:10:30 +0900

    0813_tailwind

---
+++ 15-style-with-tailwind/src/App.tsx
@@ -0,0 +1,13 @@
+
+import '/src/styles/style.css'
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className='flex justify-center items-center bg-indigo-500 text-white px-4 py-2 text-shadow-lg'>hello tailwind</h1>
+    </div>
+  )
+}
+export default App
